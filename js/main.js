fe8ac994846fdaec4e1bec7511d5adff2f34e812
regiCesar21 <regisson.cesar21@gmail.com>
2020-12-22 10:32:45 -0300

    aula 2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var idade = 21;
+/*var idade = 21;
 var idade2 = 10;
 var nome = "Regisson Cesar";
 var frase = "exemplo";
@@ -6,3 +6,16 @@ var frase = "exemplo";
 //alert(idade + idade2);
 console.log(idade + idade2);
 console.log(frase.replace("exemplo", "Mudando a palavra"));
+
+
+var lista = ["Maçã", "Pera", "Laranja"];
+console.log(lista[0]);
+alert(lista[1]);
+lista.push("uva");
+console.log(lista);
+lista.pop();
+console.log(lista.join(" - "));
+*/
+
+var cadastro = [{nome: "Regisson" , idade: 21}, {nome: "pedro" , idade: 20}]
+console.log(cadastro);
