3f6e7cf61311ea8787983360b742206c1caa86b7
regiCesar21 <regisson.cesar21@gmail.com>
2020-12-22 10:11:55 -0300

    aula 1

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var idade = 21;
+var idade2 = 10;
+var nome = "Regisson Cesar";
+var frase = "exemplo";
+//alert("nome: " +nome + " Idade: "+idade);
+//alert(idade + idade2);
+console.log(idade + idade2);
+console.log(frase.replace("exemplo", "Mudando a palavra"));
