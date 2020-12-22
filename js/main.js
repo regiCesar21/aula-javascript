9aa889c41566383b315b43ba894044f0da3d9b23
regiCesar21 <regisson.cesar21@gmail.com>
2020-12-22 10:53:49 -0300

    aula 3

--- js/main.js
+++ js/main.js
@@ -15,7 +15,33 @@ lista.push("uva");
 console.log(lista);
 lista.pop();
 console.log(lista.join(" - "));
-*/
+
 
 var cadastro = [{nome: "Regisson" , idade: 21}, {nome: "pedro" , idade: 20}]
 console.log(cadastro);
+
+
+var idade = prompt("qual sua idade?");
+
+if(idade > 18){
+    alert("Maior de idade");
+} else{
+    alert("menor de idade");
+};
+
+
+
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    count = count + 1;
+}
+
+
+var count = 0;
+
+for(count = 0; count <= 5; count++){
+    console.log(count);
+}
+
+*/
